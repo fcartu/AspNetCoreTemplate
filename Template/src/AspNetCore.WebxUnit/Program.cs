d18fd653f5a8839f9368a9b2bb7e8b3fc3ebcaa8
fcartu <fcartu@gmail.com>
2018-03-15 13:54:35 -0500

    add serilog to template

--- Template/src/AspNetCore.WebxUnit/Program.cs
+++ Template/src/AspNetCore.WebxUnit/Program.cs
@@ -1,18 +1,39 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace AspNetCore.WebxUnit
 {
     public class Program
     {
+        public static IConfiguration Configuration { get; } =
+            new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile(
+                    $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", 
+                        optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration)
+                .Enrich.FromLogContext()
+                .WriteTo.File("log.txt")
+                .CreateLogger();
+
             BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseSerilog()
                 .Build();
     }
 }
